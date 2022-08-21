d45515a8dffd06b292a2fd32ea30b3856c40663a
Nick Evans <evansnicholas@hotmail.com>
2022-08-21 19:13:28 +0100

    Initial website setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import logo from "../img/logo.png";
+
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <nav
+      className="navbar is-fixed-top"
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item" title="Logo">
+            <img src={logo} alt="Fibreshed-Scotland" className="image"/>
+          </Link>
+          {/* Hamburger menu */}
+          <button
+            className={`navbar-burger burger ${isActive && "is-active"}`}
+            aria-expanded={isActive}
+            onClick={() => setIsActive(!isActive)}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+        </div>
+        <ul id="navMenu" className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"}`}>
+            {/* TODO: inline override of padding is a result of refactoring
+                to a ul for accessibilty purposes, would like to see a css
+                re-write that makes this unneccesary.
+             */}
+            <li className="navbar-item" style={{padding: "0px"}}>
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="navbar-item" style={{padding: "0px"}}>
+            <Link className="navbar-item" to="/members">
+              Members
+            </Link>
+            </li>
+            <li className="navbar-item" style={{padding: "0px"}}>
+            <Link className="navbar-item" to="/blog">
+              Blog
+            </Link>
+            </li>
+            <li className="navbar-item" style={{padding: "0px"}}>
+            <Link className="navbar-item" to="/contact">
+              Contact
+            </Link>
+            </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
